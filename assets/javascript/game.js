8f67cee13e06b2a0e25e5d2a4e699990fd9decc8
andriyana <andriyana2212@gmail.com>
2018-10-22 18:44:28 -0400

    initial html and css

---
+++ assets/javascript/game.js
@@ -0,0 +1,10 @@
+var startScreen;
+
+function firstScreen() {
+	startScreen = "<p class='buttons'><a class='btn start-button' role='button'>Begin Quiz</a></p>";
+	$("#screen").html(startScreen);
+}
+
+// href='#' //
+
+firstScreen();
